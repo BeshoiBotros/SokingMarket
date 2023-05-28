f4804a0272b4cdc08061d1787fbe1b9b8d42d9a6
BeshoiBotros <beshoibotros111@gmail.com>
2023-05-28 14:45:16 +0300

    Beshoi

--- user/models.py
+++ user/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 import uuid
-from django.contrib.auth.hashers import make_password
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
