af2cb28c9a9325147759cbc725be2c2c100991ff
BeshoiBotros <beshoibotros111@gmail.com>
2023-05-29 01:38:53 +0300

    FFF

--- Market/settings.py
+++ Market/settings.py
@@ -67,7 +67,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'beshoibotros111@gmail.com'
-EMAIL_HOST_PASSWORD = 'fhkayzteznseznxq'
+EMAIL_HOST_PASSWORD = '###########3'
 EMAIL_USE_TLS = True
 
 CORS_ORIGIN_ALLOW_ALL = True
